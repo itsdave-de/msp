51112803927d3afd8f65ca09fbd582ed68103312
root <root@erpnext.itsdave.de>
2021-03-24 13:32:26 +0100

    added copy password to clipboard for IT User Account added role IT User Account Manager

--- msp/msp/doctype/it_user_account/it_user_account.js
+++ msp/msp/doctype/it_user_account/it_user_account.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IT User Account', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+        frm.add_custom_button('Copy PW', () => frm.trigger('get_pw'));
 
-	// }
+    },
+    get_pw: function(frm) {
+        frm.call('copy_pw', {
+            'user_agent': navigator.userAgent,
+            'platform': navigator.platform,
+        },
+        (r) => {
+			frm.events.CopyToClipboard(r.message)
+        }
+        );
+    },
+	CopyToClipboard: function(value) {
+		var tempInput = document.createElement("input");
+		tempInput.value = value;
+		document.body.appendChild(tempInput);
+		tempInput.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempInput);
+	},
+	  
+
+
+
+   
 });
+ 
--- msp/msp/doctype/it_user_account/it_user_account.py
+++ msp/msp/doctype/it_user_account/it_user_account.py
@@ -3,8 +3,23 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ITUserAccount(Document):
-	pass
+
+    @frappe.whitelist()
+    def copy_pw(self, user_agent, platform):
+        """
+        pw_access_doc = frappe.get_doc({
+            "doctype": "User Account Password Access",
+            "access_datetime": frappe.utils.datetime.datetime.now(),
+            "user": frappe.session.user,
+            "ip": frappe.local.request_ip,
+            "method": "copy_pw",
+            "platform": platform,
+            "user_agent": user_agent})
+        self.append("access_history", pw_access_doc)
+        self.save()
+        """
+        return(self.get_password("password"))
