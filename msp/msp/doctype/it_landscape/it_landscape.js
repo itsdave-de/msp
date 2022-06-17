30ce33e2439ad505f4c47d8b15d453327192aa31
Dave <dev@itsdave.de>
2022-06-17 11:36:27 +0200

    open Links to Monitoring and Ticket System

--- msp/msp/doctype/it_landscape/it_landscape.js
+++ msp/msp/doctype/it_landscape/it_landscape.js
@@ -4,7 +4,20 @@
 frappe.ui.form.on('IT Landscape', {
 	refresh: function(frm) {
 			frm.add_custom_button('Copy SSH Keys', () => frm.trigger('copy_ssh_keys'), 'Actions');
+			if (frm.doc.ticket_system_link != "") {
+				frm.add_custom_button('Open Ticket System', () => frm.trigger('open_ticket_system'), 'Actions');
+			};
+			if (frm.doc.monitoring_link != "") {
+				frm.add_custom_button('Open Monitoring', () => frm.trigger('open_monitoring'), 'Actions');
+			}
 		},
+	open_ticket_system: function(frm) {
+		window.open(frm.doc.ticket_system_link, '_blank').focus();
+	},
+	open_monitoring: function(frm) {
+		window.open(frm.doc.monitoring_link, '_blank').focus();
+	},
+
 	copy_ssh_keys: function(frm) {
 		frappe.call({
 				"method": "msp.whitelisted_tools.get_ssh_keys_for_landscape",
