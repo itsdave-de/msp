48ca998883a1ee3838834ab69f68d145d7016951
Dave <dev@itsdave.de>
2022-06-17 11:55:41 +0200

    Admin Interface and monitoring links

--- msp/msp/doctype/it_object/it_object.js
+++ msp/msp/doctype/it_object/it_object.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IT Object', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.admin_interface_link) {
+			frm.add_custom_button('Open Admin Interface', () => frm.trigger('open_admin_interface'), 'Actions');
+		};
+		if (frm.doc.monitoring_link) {
+			frm.add_custom_button('Open Monitoring', () => frm.trigger('open_monitoring'), 'Actions');
+		};
+	},
+	open_admin_interface: function(frm) {
+		window.open(frm.doc.admin_interface_link, '_blank').focus();
+	},
+	open_monitoring: function(frm) {
+		window.open(frm.doc.monitoring_link, '_blank').focus();
+	},
+	
 });
