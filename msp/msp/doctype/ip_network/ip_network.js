b49b98851d19ffbb2bc9c69af5dde74ddcd20530
Jordi Albert <63541019+jarg1023@users.noreply.github.com>
2022-08-18 14:35:19 +0200

    Add IP addresses usage information for an IP network in IP network view

--- msp/msp/doctype/ip_network/ip_network.js
+++ msp/msp/doctype/ip_network/ip_network.js
@@ -14,5 +14,46 @@ frappe.ui.form.on('IP Network', {
 				}
 			})
 		})
+	},
+	onload_post_render(frm) {
+		getUsedIpsInNetwork(frm);
+	},
+	after_save(frm) {
+		getUsedIpsInNetwork(frm);
 	}
 });
+
+function getUsedIpsInNetwork(frm) {
+	frm.call('get_used_ips', {})
+		.then((response) => {
+			const container = document.getElementById("usage-overview-table");
+			let tableBody = ``;
+
+			response?.message?.forEach((element) => {
+				tableBody += `
+						<tr>
+							<td style="border: 1px solid #000; padding: 0.5rem 1rem; text-align: left;">${element?.ip_address ?? "-"}</td>
+							<td style="border: 1px solid #000; padding: 0.5rem 1rem; text-align: left;">${element?.title ?? "-"}</td>
+							<td style="border: 1px solid #000; padding: 0.5rem 1rem; text-align: left;">${element?.type ?? "-"}</td>
+						</tr>
+					`;
+			});
+
+			const table = tableBody ? `
+					<table style="border-collapse: collapse; border: 2px solid #000; text-align: left; width: 100%; color: #000;">
+						<thead>
+							<tr>
+								<th scope="col" style="border: 1px solid #000; padding: 0.5rem 1rem;">IP</th>
+								<th scope="col" style="border: 1px solid #000; padding: 0.5rem 1rem;">Name</th>
+								<th scope="col" style="border: 1px solid #000; padding: 0.5rem 1rem;">Type</th>
+							</tr>
+						</thead>
+						<tbody>
+							${tableBody}
+						</tbody>
+					</table>
+				` : 'No IPs used for this network';
+
+			container.innerHTML = table;
+		})
+}
--- msp/msp/doctype/ip_network/ip_network.py
+++ msp/msp/doctype/ip_network/ip_network.py
@@ -8,7 +8,34 @@ from ipaddress import IPv4Address, IPv4Network
 from frappe.model.document import Document
 
 class IPNetwork(Document):
-	pass
+	@frappe.whitelist()
+	def get_used_ips(self):
+		values = {'ip_network': self.name}
+		result = []
+		result = frappe.db.sql("""
+			SELECT
+				ipa.ip_address,
+				ito.title,
+				ito.type
+			FROM `tabIP Address` ipa
+				JOIN `tabIT Object` ito
+				ON ipa.it_object = ito.name
+			WHERE ipa.ip_network = %(ip_network)s
+		""", values=values, as_dict=1)
+		
+		for ip_network_reserved_range in self.ip_network_reserved_ranges_table:
+			result.append({
+				'ip_address': ip_network_reserved_range.start,
+				'title': ip_network_reserved_range.type,
+				'type': 'DHCP Range Start'
+			})
+			result.append({
+				'ip_address': ip_network_reserved_range.end,
+				'title': ip_network_reserved_range.type,
+				'type': 'DHCP Range End'
+			})
+
+		return result
 
 @frappe.whitelist()
 def calculate_network_data(doc):
